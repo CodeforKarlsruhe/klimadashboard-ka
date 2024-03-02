ba10978985e69fea795ebd1957d61514d3fbf56c
Nils Osswald <contact@nilsosswald.de>
2024-03-02 15:01:51 +0100

    Style landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import ExampleChart from "@/components/example-chart";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-zinc-200 text-black">
-      <h1>Klimadashboard Karlsruhe</h1>
+      <h1 className="mb-12 text-4xl font-bold">
+        Klimadashboard der Stadt <span className="text-sky-500">Karlsruhe</span>
+      </h1>
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-12">
         <ExampleChart />
         <PenguinChart />
