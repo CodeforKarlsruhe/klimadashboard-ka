25c86d273d94323786a437e3462753e115505117
Kai Jellinghaus <contact@kaij.tech>
2024-03-02 12:00:14 +0100

    SWR Sample Data

---
+++ src/app/data.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import useSWR from "swr";
+
+const fetcher = (
+  input: URL | RequestInfo,
+  init?: RequestInit | undefined,
+) => fetch(input, init).then((res) => res.json());
+
+function useData1() {
+  const { data, error, isLoading } = useSWR(`/api/test-data`, fetcher);
+
+  return {
+    data: data,
+    isLoading,
+    isError: error,
+  };
+}
