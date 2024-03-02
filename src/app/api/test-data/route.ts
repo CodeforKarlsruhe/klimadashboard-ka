bd7e24208613e322e95c049293d494df842afdcb
Kai Jellinghaus <contact@kaij.tech>
2024-03-02 12:44:22 +0100

    Data 3

--- src/app/api/test-data/route.ts
+++ src/app/api/test-data/route.ts
@@ -1,16 +1,41 @@
 import * as d3 from "d3";
+import * as geo from "d3-geo";
+import * as dsv from "d3-dsv";
 import { NextRequest, NextResponse } from "next/server";
+
+const fmt = dsv.dsvFormat(";");
 
 export async function GET(
   req: NextRequest,
 ): Promise<NextResponse> {
-  const data = await d3.csv(
+  const response = await fetch(
     "https://transparenz.karlsruhe.de/dataset/ad0659b0-9e4b-408a-8840-3f19d5ec743d/resource/df843a83-b3f6-4929-b6a8-96471508f6f2/download/sensordaten_karlsruhe_bodentemperatur_bodenfeuchte_oct_2023.csv",
   );
 
-  console.log(data);
+  const data = fmt.parse(await response.text());
+
+  const data2 = d3.map(
+    data,
+    (row) => ({
+      "bodentemperatur": row["bodentemperatur"],
+      "combinedTime": Date.parse(
+        row["Datum"].split("-").toReversed().join("-") + "T" + row["Uhrzeit"] +
+          ":00.000Z",
+      ),
+    }),
+  );
+
+  const data3 = d3.map(
+    data2,
+    (row) => ({
+      time: row["combinedTime"],
+      bodentemperatur: Number.parseFloat(
+        row["bodentemperatur"].replace(",", "."),
+      ),
+    }),
+  );
 
   return NextResponse.json({
-    data: [[Date.now() - 10000, 10], [Date.now(), 1]],
+    data: data3,
   });
 }
--- src/components/example-chart.tsx
+++ src/components/example-chart.tsx
@@ -4,35 +4,32 @@ import { ElementRef, useEffect, useRef } from "react";
 import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 import { useData1 } from "@/app/data";
+import PlotFigure from "./PlotFigure";
 
 const ExampleChart: React.FC = () => {
   const chartRef = useRef<ElementRef<"div">>(null);
 
   const { data, isError, isLoading } = useData1();
 
-  useEffect(() => {
-    if (isLoading || isError || !data) {
-      return;
-    }
+  if (isLoading) return <></>;
 
-    const mappedData = Object.values(data).map((d: any) => ({
-      a: d[0],
-      b: d[1],
-    }));
-
-    const chart = Plot.plot({
-      x: { padding: 0.4 },
-      marks: [Plot.barY(mappedData, { x: "a", y: "b", fill: "green" })],
-    });
-
-    chartRef.current?.append(chart);
-    return () => chart.remove();
-  }, [data, isError, isLoading]);
+  console.log(data.data);
 
   return (
-    <div className="bg-white text-black" ref={chartRef}>
-      Data: {JSON.stringify(data)}
-    </div>
+    <PlotFigure
+      options={{
+        x: { padding: 0.4 },
+        marks: [
+          Plot.barY(data.data, {
+            x: "time",
+            y: "bodentemperator",
+            fill: "green",
+            interval: d3.utcDay,
+          }),
+        ],
+      }}
+    >
+    </PlotFigure>
   );
 };
 
