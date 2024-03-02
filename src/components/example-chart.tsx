35f9d6efd78da20adedff605746f62a512d5bedb
Nils Osswald <contact@nilsosswald.de>
2024-03-02 12:29:39 +0100

    Display example data

--- src/components/example-chart.tsx
+++ src/components/example-chart.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { ElementRef, useEffect, useRef, useState } from "react";
+
+import { ElementRef, useEffect, useRef } from "react";
 import * as Plot from "@observablehq/plot";
 import * as d3 from "d3";
 import { useData1 } from "@/app/data";
@@ -9,20 +10,30 @@ const ExampleChart: React.FC = () => {
 
   const { data, isError, isLoading } = useData1();
 
-  useEffect(() => {}, []);
-
   useEffect(() => {
     if (isLoading || isError || !data) {
       return;
     }
 
-    const chart = Plot.plot({});
+    const mappedData = Object.values(data).map((d: any) => ({
+      a: d[0],
+      b: d[1],
+    }));
+
+    const chart = Plot.plot({
+      x: { padding: 0.4 },
+      marks: [Plot.barY(mappedData, { x: "a", y: "b", fill: "green" })],
+    });
 
     chartRef.current?.append(chart);
     return () => chart.remove();
   }, [data]);
 
-  return <div ref={chartRef}>Data: {data}</div>;
+  return (
+    <div className="bg-white text-black" ref={chartRef}>
+      Data: {JSON.stringify(data)}
+    </div>
+  );
 };
 
 export default ExampleChart;
